764e812f008f245673218efb804200c74b528eb4
ochanman <david.godignon@lapiscine.pro>
2021-12-18 19:56:30 +0100

    cardinalité 1

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -37,6 +37,11 @@ class Product
      */
     private $warranty;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     */
+    private $user;
+
 
 
     public function getId(): ?int
@@ -88,6 +93,18 @@ class Product
     public function setWarranty(string $warranty): self
     {
         $this->warranty = $warranty;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
--- src/Entity/Repair.php
+++ src/Entity/Repair.php
@@ -27,6 +27,16 @@ class Repair
      */
     private $description;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Product::class)
+     */
+    private $product;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -52,6 +62,30 @@ class Repair
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): self
+    {
+        $this->product = $product;
 
         return $this;
     }
